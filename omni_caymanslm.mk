f3439e4499dce8022d56cbac6af05226e69fec87
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2023-06-05 20:35:42 +0200

    caymanslm: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: arm64 Manufacturer: lge Model: LM-G910 Script version: 3.0.0

---
+++ omni_caymanslm.mk
@@ -0,0 +1,29 @@
+#
+# Copyright (C) 2023 The Android Open Source Project
+# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+
+# Inherit from caymanslm device
+$(call inherit-product, device/lge/caymanslm/device.mk)
+
+PRODUCT_DEVICE := caymanslm
+PRODUCT_NAME := omni_caymanslm
+PRODUCT_BRAND := lge
+PRODUCT_MODEL := LM-G910
+PRODUCT_MANUFACTURER := lge
+
+PRODUCT_GMS_CLIENTID_BASE := android-lge
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="caymanslm-user 11 RKQ1.210420.001 2221520109237 release-keys"
+
+BUILD_FINGERPRINT := lge/caymanslm/caymanslm:11/RKQ1.210420.001/2221520109237:user/release-keys
